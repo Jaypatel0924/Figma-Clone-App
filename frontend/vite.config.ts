7c33fb1163f8813b450736f4807dcb044888e4bb
Jaypatel0924 <jayshubham2324@gmail.com>
2025-11-24 13:26:28 +0530

    Fix rollup manualChunks for Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -28,35 +28,4 @@
 //     }
 //   }
 // })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('lucide-react')) {
-              return 'lucide';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://figma-clone-app-gnsu.vercel.app/api',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
