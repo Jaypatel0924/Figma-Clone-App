dcada059fa71b8de4a7045b79bfad4ceadc8e41b
Jaypatel0924 <jayshubham2324@gmail.com>
2025-11-24 13:22:01 +0530

    vite config change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,39 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     outDir: 'dist',
+//     sourcemap: false,
+//     minify: 'terser',
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom', 'react-router-dom'],
+//           lucide: ['lucide-react']
+//         }
+//       }
+//     }
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://figma-clone-app-gnsu.vercel.app/api',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -21,7 +50,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://figma-clone-app-gnsu.vercel.app/api',
         changeOrigin: true,
         secure: false
       }
