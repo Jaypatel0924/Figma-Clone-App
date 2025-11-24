2e95667d16de65db3a568f0db7da56b5071bab29
Jaypatel0924 <jayshubham2324@gmail.com>
2025-11-24 13:24:26 +0530

    vite config change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -31,7 +31,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -40,9 +39,13 @@ export default defineConfig({
     minify: 'terser',
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          lucide: ['lucide-react']
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('lucide-react')) {
+              return 'lucide';
+            }
+            return 'vendor';
+          }
         }
       }
     }
